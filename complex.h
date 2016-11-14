ed41d1ebb5f7e9b69fcc725715eba39fc5be0a33
Harrison Tran <atxharrison.tran@gmail.com>
2016-11-14 10:25:41 -0600

    Add header file, executable
    Add headers for complex functions and add a test file with associated
    binary for testing these functions.

---
+++ complex.h
@@ -0,0 +1,33 @@
+// Start header
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+// Declarations
+double abs(const complex c);
+double arg(const complex c);
+complex conj(const complex c);
+double imag(const complex& c);
+double norm(const complex c);
+complex polar(double mag m, double ang=0.0);
+double real(const complex& c);
+
+complex acos(const complex c);
+complex asin(const complex c);
+complex atan(const complex c);
+complex cos(const complex c);
+complex cosh(const complex c);
+complex exp(const complex c);
+complex log(const complex c);
+complex log10(const complex c);
+complex pow(double b, const complex exp);
+complex pow(const complex b, int exp);
+complex pow(const complex b, double exp);
+complex pow(const b, const complex exp);
+complex sin(const complex c);
+complex sinh(const complex c);
+complex sqrt(const complex c);
+complex tan(const complex c);
+complex tanh(const complex c);
+
+// End header file
+#endif
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <complex.h>
+using namespace std;
+int main() {
+	cout << "This is a test of the complex library\n";
+	
+}
