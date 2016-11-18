d1d65b4048a45b0372586ada1008e92af93510f5
ethanatortx <ethan@riversfamily.us>
2016-11-18 11:20:06 -0600

    ethan io temp

--- io temp.cpp
+++ io temp.cpp
@@ -1,3 +1,22 @@
 // maybe more elegant and simple solution to io problem
 
-#include <complex.h>;
+#include "complex.h"
+
+using namespace std;
+
+double realArr[2] = {0.0,0.0};
+double imaginaryArr[2] = {0.0,0.0};
+
+
+istream& operator>>(istream& lhs, complex& rhs) {
+	int i = 0;
+	while (lhs.good()) {
+		char temp = lhs.get();
+		cout << temp;
+		/*if (atof(temp)!=0.0) {
+			realArr[i] = atof(temp);
+			i++
+		}*/
+	}
+	cout << realArr[0] << realArr[1] << endl;
+}
