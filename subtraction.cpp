a1f5811414c74f696f6b4eb8b6235df8019d13f2
ethanatortx <ethan@riversfamily.us>
2016-11-18 10:03:47 -0600

    subtraction

--- subtraction.cpp
+++ subtraction.cpp
@@ -1,3 +1,12 @@
 #include "complex.h"
 
 // do stuff
+complex operator-(complex a, complex b) {
+	complex x;
+	
+	x.real = a.real -= b.real;
+	
+	x.imaginary = a.imaginary -= b.imaginary;
+
+	return x;
+}
